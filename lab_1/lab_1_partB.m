e0c87dcf128181b8b8d1e7b222a9bf15e2dc8537
Matthew Kim <32852290+HyprValent@users.noreply.github.com>
2022-01-23 16:56:46 -0800

    finished task B

---
+++ lab_1/lab_1_partB.m
@@ -0,0 +1,56 @@
+%% 1. Input arguments of audiorecorder
+
+% The input arguments of audiorecorder are as follows:
+
+% Fs - a double that denotes the sampling frequency of the recording in Hz. The
+% sampling rate can vary from system to system, but it has to be between
+% 1000 Hz and 384000 Hz as restricted by MATLAB
+
+% nBits - an integer that denotes bits per sample. The default is 8 bits
+% but can be configured only else to 16 and 24 if there are floating point
+% values in signal samples (since floating points require extra bits).
+
+% NumChannels - an integer that denotes the number of channels the signal 
+% is recorded in. The value can only be either 1 for mono output (set by default), or 2
+% for stereo output
+
+% ID - an integer denoting the ID of a device that will be used for audio
+% input. It is set to -1 by default, although it can be configured to a
+% specified ID produced by the audiodevinfo function
+
+%% 2. Recording the audio
+recObj = audiorecorder(44100,8,1);
+recordblocking(recObj, 3);
+
+%% Saving and playing the audio
+sampleSpeech = getaudiodata(recObj);
+timeAxis = (0:length(sampleSpeech)-1)*3 / length(sampleSpeech);
+figure(1)
+plot(timeAxis, sampleSpeech);
+title("Sample Speech");
+% play(recObj);
+
+% Without changing the timeAxis manually, the current grpah is plotted
+% with a domain [0,14*10^4] based on the number of samples. The signal is
+% discrete in time and amplitude
+
+%% 3. changing the sampling frequency to 1000Hz
+recObj_downsampled = audiorecorder(1000,8,1);
+recordblocking(recObj_downsampled, 3);
+
+%% Saving and playing the audio
+sampleSpeech_downsampled = getaudiodata(recObj_downsampled);
+timeAxis_downsampled = (0:length(sampleSpeech_downsampled)-1)*3 / length(sampleSpeech_downsampled);
+figure(2)
+plot(timeAxis_downsampled, sampleSpeech_downsampled);
+title("Sample Speech Downsampled");
+% play(recObj_downsampled);
+
+% The downsampled frequency sounds more muffled and quiet than the original
+% one. When observing the graph it also seems as though there is less
+% detail/variation in the downsampled version due to less samples being
+% recorded per second.
+
+% This means that there is a loss of information when we choose to
+% downsample, meaning that a lot of the voice going through the recording
+% is cut out, causing the muffled/quieter output.
